e75554ff3c93d0a996f4fa25724be6bc35800cf0
Varelilla <varelilla1@gmail.com>
2024-05-15 09:50:44 +0200

    PROBANDO
    Co-authored-by: mapraiz <mapraiz@users.noreply.github.com>

---
+++ menu.php
@@ -0,0 +1,30 @@
+<div id="menu">
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+           
+            <li><a href="leaderboard.html">Leaderboard</a></li>
+
+            <li><a href="discografia.html">Discografía</a></li>
+            
+            <li><a href="registro.html">Registro:</a> </li>
+            <li><a href="login.html">Login</a></li>
+            
+            
+        </ul>
+        <ul class="right">
+            <?php 
+              session_start();
+              $user = $_SESSION["user"];
+            if (!isset($_SESSION['id_usuario'])) {
+                echo '<li><a href="login.html">Login</a></li>';
+            } else {
+                echo '<li><a href="logout.php">Logout</a></li>';
+                echo '<li><a href="gestion.php">Gestión</a></li>';
+                echo '<li><a href="buscador.php">Buscador</a></li>';
+                echo '<li><a href="buscar_noticias_fecha.php">Buscar por fecha</a></li>';
+            }
+            ?>
+        </ul>
+    </nav>
+</div>
