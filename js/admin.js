6113db01ab0f793bce1f9d34e2d60c5b861b7fc5
mapraiz <mapraizclase@gmail.com>
2024-05-27 10:36:30 +0200

    qtqtqert

---
+++ js/admin.js
@@ -0,0 +1,36 @@
+
+$(document).ready(function(){
+
+    $(".editButton")
+
+
+
+
+    $(".deleteButton").on("click", function(event){
+        console.log("hello");
+        swal({
+            title:"Estas seguro?",
+            text:"No podras revertir el cambio",
+            icon:"warning",
+            buttons:{
+                cancel:"No",
+                confirm:{
+                    text:"Si",
+                    value:"confirm"
+                }
+
+            }   
+        }).then(value){
+            if(value=="confirm"){
+                $.ajax({
+                    url: "http://127.0.0.1/PhotoPlay-Web/servicios.php",
+                    type: "POST",
+                    data: { function: "deleteScore", username:username, newUsername:newUsername},
+                    success: function(data){
+                        
+                    }
+                });
+            }
+        }
+    })
+})
