77d5b30fe60879d493f4949c1a7352079feef387
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-27 11:07:34 +0200

    gdzf

--- login.php
+++ login.php
@@ -126,6 +126,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/jquery-3.7.1.min.js"></script>
+    //prueba
+    <script src="js/login.js"></script>
+    
     
 
 </body>
