5aaa4a6e374dfd38e3b5b5f6019ae2b0ca143009
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-28 11:49:18 +0200

    ds

--- login.php
+++ login.php
@@ -127,7 +127,6 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="js/jquery-3.7.1.min.js"></script>
-<script src="js/login.php"></script>
 
 </body>
 </html>
