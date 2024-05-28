1ae9ffda4647a5a3d510529c3472b3de79de4627
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-28 10:25:56 +0200

    n

--- registro.php
+++ registro.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
 
     <title>PhotoPlay</title>   
-    <link rel="stylesheet" href="css/comun.css">
     <link rel="stylesheet" href="css/registro.css">
 </head>
 <body>
@@ -35,7 +34,7 @@
         <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card bg-custom-1 text-white " style="border-radius: 1rem;" style="background-color: #685bd9;" >
+            <div class="card bg-custom-1 text-white " style="border-radius: 1rem;"  >
                 <div class="card-body p-5 text-center">
     
                 <div class="mb-md-5 mt-md-4 pb-5">
