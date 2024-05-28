000b8583311abf6b30264b7f41c4b3e4df028d3f
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-28 09:28:41 +0200

    dsa

---
+++ prueba.php
@@ -0,0 +1,15 @@
+<?php
+$db_username = 'c##photoplay';
+$db_password = 'almi123';
+$db_service = '3.221.255.12:1521/ORCLCDB';
+
+$connection = oci_connect($db_username, $db_password, $db_service);
+
+if (!$connection) {
+    $error = oci_error();
+    echo "Error de conexión: " . htmlspecialchars($error['message']);
+} else {
+    echo "Conexión exitosa!";
+    oci_close($connection);
+}
+?>
