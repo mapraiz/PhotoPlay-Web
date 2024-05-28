90cbcf3bd8539e76a02962d1acc72a02d693321d
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-28 13:00:41 +0200

    vx

--- prueba.php
+++ prueba.php
@@ -38,7 +38,7 @@
                             // Configuración de la conexión a la base de datos Oracle
                             $db_username = 'c##photoplay';
                             $db_password = 'almi123';
-                            $db_service = '3.221.255.12:1521/ORCLCDB';
+                            $db_service = '3.221.255.12:8080/ORCLCDB';
 
                             // Realizar la conexión a la base de datos Oracle
                             $connection = oci_connect($db_username, $db_password, $db_service);
