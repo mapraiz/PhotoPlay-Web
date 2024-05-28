8593a16013ba65a9ee99e7ba282108f75c466cd0
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-28 10:49:01 +0200

    fd

--- registro.php
+++ registro.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <title>PhotoPlay</title>   
+    <link rel="stylesheet" href="css/comun.css">
     <link rel="stylesheet" href="css/registro.css">
 </head>
 <body>
@@ -31,12 +32,11 @@
                 <div class="card bg-custom-1 text-white" style="border-radius: 1rem;">
                     <div class="card-body p-5 text-center">
                         <div class="mb-md-5 mt-md-4 pb-5">
-                            <h2 class="fw-bold mb-2 text-uppercase">Registro</h2>
-                            <p class="text-white-50 mb-5">Inserte sus datos</p>
+                            <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                            <p class="text-white-50 mb-5">Please enter your login and password!</p>
 
-                          
 
-                            <form action="registro.php" method="post">
+                            <form action="login.php" method="get">
                                 <div class="form-outline form-white mb-4">
                                     <input type="text" id="typeUsernameX" name="username" class="form-control form-control-lg" required />
                                     <label class="form-label" for="typeUsernameX">Username</label>
@@ -45,9 +45,6 @@
                                 <div class="form-outline form-white mb-4">
                                     <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg" required />
                                     <label class="form-label" for="typePasswordX">Password</label>
-                                    <input type="password" id="typePasswordX" name="password" class="form-control form-control-lg" required />
-                                    <label class="form-label" for="typePasswordX">Password</label>
-                                
                                 </div>
 
                                 <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
@@ -85,6 +82,7 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="js/jquery-3.7.1.min.js"></script>
+<script src="js/login.php"></script>
 
 </body>
 </html>
