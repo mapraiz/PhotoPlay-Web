f66d2cd27638230cb2b33e17cf3df0bb9ff46559
testm <test@example.com>
2024-05-28 17:34:00 +0200

    asd

--- test_oracle.php
+++ test_oracle.php
@@ -1,11 +1,18 @@
 <?php
-$conn = oci_connect('c##photoplay', 'almi123', '3.221.255.12:1521/ORCLCDB');
+try {
+    $conn = oci_connect('c##photoplay', 'almi123', '3.221.255.12:1521/ORCLCDB');
+    
+    if (!$conn) {
+        $e = oci_error();
+        throw new Exception(htmlentities($e['message'], ENT_QUOTES));
+    }
 
-if (!$conn) {
-    $e = oci_error();
-    trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+    echo "Conexión a Oracle establecida correctamente";
+} catch (Exception $e) {
+    echo "No se pudo establecer la conexión a Oracle: " . $e->getMessage();
+} finally {
+    if ($conn) {
+        oci_close($conn);
+    }
 }
-
-echo "Conexión a Oracle establecida correctamente";
-oci_close($conn);
 ?>
