0ddc701178539f5164934f738789b4dfced3e64c
mihaiclasedam <mihaiclasedam@gmail.com>
2024-05-28 12:05:10 +0200

    h

--- prueba.php
+++ prueba.php
@@ -1,4 +1,12 @@
 <?php
+if (extension_loaded('oci8')) {
+    echo "OCI8 está habilitado.";
+} else {
+    echo "OCI8 no está habilitado.";
+}
+?>
+
+<?php
 $db_username = 'c##photoplay';
 $db_password = 'almi123';
 $db_service = '3.221.255.12:1521/ORCLCDB';
