ea2ddcf8fdaf0d20510541326d16f7e9d046bd26
diegoreto7u7 <diegoret2005@gmail.com>
2024-05-29 10:40:02 +0200

    adsad

--- login.php
+++ login.php
@@ -35,7 +35,7 @@
 
                                         <?php
                                         session_start();
-
+                                        var_dump("ostia1"); // Debugging
                                         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                                             $username = $_POST['username'];
                                             $password = $_POST['password'];
